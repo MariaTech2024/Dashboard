d1d63c30df193755ca56d571f739f0720b308175
mariaMeNow <mariiamenow@gmail.com>
2025-02-12 11:43:52 +0200

    add analytics page and its components

---
+++ src/pages/AnalyticsPage.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Header from "../components/layout/Header.jsx";
+import OverviewCards from "../components/analytics/OverviewCards.jsx";
+import RevenueChart from "../components/analytics/RevenueChart.jsx";
+import ChannelPerformance from "../components/analytics/ChannelPerformance.jsx";
+import ProductPerformance from "../components/analytics/ProductPerformance.jsx";
+import UserRetention from "../components/analytics/UserRetention.jsx";
+import CustomerSegmentation from "../components/analytics/CustomerSegmentation.jsx";
+import AIPoweredInsights from "../components/analytics/AIPoweredInsights.jsx";
+
+const AnalyticsPage = () => {
+	return (
+		<div className='flex-1 overflow-auto relative z-10 bg-gray-900'>
+			<Header title={"Analytics"} />
+
+			<main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
+				<OverviewCards />
+				<RevenueChart />
+
+				<div className='grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8'>
+					<ChannelPerformance />
+					<ProductPerformance />
+					<UserRetention />
+					<CustomerSegmentation />
+				</div>
+
+				<AIPoweredInsights />
+			</main>
+		</div>
+	);
+};
+export default AnalyticsPage;
